2556fb6fb9bd8b0c209ef0ac737814d42f7860d2
Deni Begaj <denibegaj98@gmail.com>
2020-07-14 22:29:29 +0200

    Finished implementing the contact form mailto functionality

--- js/index.js
+++ js/index.js
@@ -11,19 +11,49 @@ function getHTMLElement(id) {
 }
 
 function onContactFormSubmitted() {
-    const name = getHTMLElement('name');
-    const email = getHTMLElement('email');
-    const subject = getHTMLElement('subject');
-    const message = getHTMLElement('message');
-    alert("The form was submitted");
-    console.log(countryCodes);
+    const name = getHTMLElement('name').value;
+    if (name.va === null || name === undefined || name === '') {
+        getHTMLElement('error-message').innerText = 'Your name is required';
+        return;
+    }
+    const email = getHTMLElement('email').value;
+    if (email === null || email === undefined || email === '') {
+        getHTMLElement('error-message').innerText = 'Your email is required';
+        return;
+    }
+    const prefix = getHTMLElement('prefix').value;
+    const phone = getHTMLElement('phone').value;
+    if (prefix === null || prefix === undefined || prefix === '') {
+        getHTMLElement('error-message').innerText = 'Your prefix is required';
+        return;
+    } else if (phone === null || phone === undefined || phone === '') {
+        getHTMLElement('error-message').innerText = 'Your phone is required';
+        return;
+    }
+    const subject = 'Deni-Begaj Website: Request for discussion from ' + name;
+    const message = getHTMLElement('message').value;
+    if (message === null || message === undefined || message === '') {
+        getHTMLElement('error-message').innerText = 'A message is required to submit inquiry';
+        return;
+    }
+    const nowDate = new Date().toLocaleDateString();
+    const fullMessage = 'Request from Website:' + ' \n\n\n' +
+                        message + '\n\n\n' +
+                        'Visitor Name: ' + name + '\n' +
+                        'Visitor Phone:' + encodeURI(prefix + phone) + '\n' +
+                        'Inquiry Date: '  + nowDate;
+    const mailToString = 'mailto:' + myEmail + '?subject=' + encodeURI(subject) + '&body=' + encodeURI(fullMessage);
+
+    window.open(mailToString, '_blank');
+    getHTMLElement('success-message').innerText = 'Your email is ready to be sent. Check the next tab.';
+    return false;
 }
 
 function addCountryCodesToSelect() {
     const prefixSelect = getHTMLElement('prefix');
     for(const prefix of countryCodes) {
         const option = document.createElement('option');
-        option.text = prefix.name;
+        option.text = prefix.name + ' (' + prefix.dial_code + ')';
         option.value = prefix.dial_code;
         prefixSelect.add(option);
     }
@@ -38,6 +68,7 @@ updateHTMLElement('my-age', years);
 
 
 // Constants
+const myEmail = 'denibegaj98@gmail.com';
 const website = 'deni-begaj.github.io';
 updateHTMLElement('my-website', website);
 
